3a02ce44da7a3aabc39298dd1276734838b1b5e2
Juan Chavez <juan@Juans-Air-12.home>
2021-12-17 14:04:19 -0800

    search history buttons/feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,22 @@ var todaysTemperatureEl = document.querySelector("#temperature");
 var todaysWindEl = document.querySelector("#wind");
 var todaysHumidityEl = document.querySelector("#humidity");
 var todaysUvEl = document.querySelector("#uv");
+var historyBtnDiv = document.querySelector(".history-button-div");
+var idCounter = 0;
+var cityHistoryArray = [];
+
+var main = function(){
+    getSearchedCities();
+}
 
 //with city name, fetch coordinates
 var formSubmitHandler = function(event){
     event.preventDefault();
     //takes the value of the user input(city name)
     var city = formContentEl.value;
+
+    createHistoryBtn(city);
+    putCityInStorage(city);
 
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ab35a0a94cc8d81e7aa6c727ef9bb2e0";
 
@@ -30,6 +40,8 @@ var formSubmitHandler = function(event){
         });
     
 }
+
+
 
 //with city coordinates, now fetch weather data
 var applyCoordinates = function(data,city){
@@ -79,7 +91,7 @@ var forecastHandler = function(data){
     for (let i = 1; i < 6; i++) {
         //loops through the Daily Data for 5 days
         var dailyData = data.daily[i];
-        // get forcast container element with cards in it
+        // gets forcast container element (as a js object)
         var forcastContainer = document.getElementsByClassName("forcast-container");
         //gets the entire card element
         var forcastCardEl = forcastContainer[0].children[i-1];
@@ -102,5 +114,74 @@ var forecastHandler = function(data){
     }
 }
 
+var createHistoryBtn = function(city){
+    //creating button
+    var historyBtnEl = document.createElement("div");
+    historyBtnEl.className = "history-button";
+    //create a p element to go inside div for history button
+    var cityNameEl = document.createElement("p");
+    //give element a unique id to add click listener
+    cityNameEl.setAttribute("unique-id", idCounter);
+    cityNameEl.innerHTML = city;
+    
+    // appending p element inside div to put the city name in the button
+    historyBtnEl.appendChild(cityNameEl);
+    //append button to the left side of page
+    historyBtnDiv.appendChild(historyBtnEl);
+    
+    // increment id for next history button element
+    idCounter++;
+}
+
+var putCityInStorage = function(city){
+    cityHistoryArray.push(city);
+    localStorage["searched-city"] = JSON.stringify(cityHistoryArray);
+    
+}
+
+var getSearchedCities = function(){
+    //if searched-city key exist in local storage, then create buttons and push cities into the history array
+    if (localStorage["searched-city"]){
+        //parses the cities to avoid having many "one character strings" ex: "salem" vs "s""a""l""e""m"
+        var searchedCityStorageArray = JSON.parse(localStorage["searched-city"]);
+
+        for (let i = 0; i < searchedCityStorageArray.length; i++) {
+            // create a button for every existing citie in the search history
+            createHistoryBtn(searchedCityStorageArray[i]);
+            // pushes city names from local storage--> into the search history array--> so the array isnt empty after refreshing page. Otherwise, with an empty array, when a user submits a new city, the local storage will replace the "old, history-search-array"(salem, portland) with a "new, history-searched-array"(woodburn). This way, search history is persistent after infinite, page refreshes
+            cityHistoryArray.push(searchedCityStorageArray[i]); 
+        }
+    }
+}
+
+var historyBtnHandler = function(city) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ab35a0a94cc8d81e7aa6c727ef9bb2e0";
+
+    //fetch city coordinates
+    fetch(apiURL)
+        .then(function(response) {
+            if (response.ok){
+                response.json().then(function(data){
+                    applyCoordinates(data,city)
+                })
+            }
+            else {
+                alert("Error: GitHub user not found.");
+            }
+        }).catch(function(error) {
+            alert("Unable to connect to GitHub");
+        });
+}
+
+var clickHistoryBtn = function(event) {
+    var clickedHistoryEl = event.target
+    cityName = clickedHistoryEl.innerHTML;
+    
+    historyBtnHandler(cityName);
+}
+
+main();
 submitBtnEl.addEventListener("click", formSubmitHandler);
+historyBtnDiv.addEventListener("click", clickHistoryBtn);
+
 
